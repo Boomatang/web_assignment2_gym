ca83ac02c3a2f5fd2aed07ea9872f4a9cea3a191
James Fitzpatrick <jimfity@gmail.com>
2018-05-06 18:51:21 +0100

    Baseline
    Grade Range: Baseline
    Member: Member model: name, email, password, address, gender, height.
    Single member loaded from yaml file.
    Assessment: Multiple Assessments associated with singler user in yaml
    file. Form to add more assessments in UX
    Trainer: None
    Reports: +Current BMI, This is a static value

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import models.Assessment;
+import models.Member;
 import play.Logger;
 import play.mvc.Controller;
 
@@ -11,8 +12,23 @@ public class Dashboard extends Controller
   public static void index()
   {
     Logger.info("Rendering Dashboard");
-    List<Assessment> assessmentList = Assessment.findAll();
-    render("dashboard.html", assessmentList);
+    Member member = Member.findByEmail("homer@simpson.com");
+    render("dashboard.html", member);
+  }
+
+  public static void addAssessment(String weight, String chest, String thigh, String upperArm, String waist, String hips) {
+
+    Assessment assessment = new Assessment(Float.valueOf(weight),
+            Float.valueOf(chest),
+            Float.valueOf(thigh),
+            Float.valueOf(upperArm),
+            Float.valueOf(waist),
+            Float.valueOf(hips));
+    Member member = Member.findByEmail("homer@simpson.com");
+    member.assessmentList.add(assessment);
+    member.save();
+    Logger.info("New assessment added");
+    redirect("/dashboard");
   }
 
 }
---
+++ app/models/Member.java
@@ -0,0 +1,40 @@
+package models;
+
+import play.db.jpa.Model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Member extends Model {
+
+    public String name, email, password, address, gender;
+    public float height, startingWeight;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    public List<Assessment> assessmentList = new ArrayList<>();
+
+    public Member(String name, String email, String password, String address, String gender, float height, float startingWeight) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.address = address;
+        this.gender = gender;
+        this.height = height;
+        this.startingWeight = startingWeight;
+    }
+
+    public float currentBMI() {
+//        TODO fill this out to return the correct result
+        return 42.66f;
+    }
+
+    public static Member findByEmail(String email){
+
+        return find("email", email).first();
+    }
+
+}
